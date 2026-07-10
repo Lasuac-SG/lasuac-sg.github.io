9bdb4ec9f9595a7c440337671ac7acb1c192f221
A R <Lasuac>
2026-07-10 23:00:18 +0800

    fix : 适配了obsidian的日期类型

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,7 +6,7 @@ const blogCollection = defineCollection({
   loader: glob({ pattern: "*.md", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
-    date: z.string(),
+    date: z.coerce.date(),
     description: z.string().optional(),
     tags: z.array(z.string()).optional()
   })
